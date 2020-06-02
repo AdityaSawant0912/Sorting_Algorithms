3b58f1688b0d93efc5302d727a6f0d0a4edec4de
AdityaSawant0912 <arsawant09@gmail.com>
2020-06-02 19:27:46 +0530

    Created Repo, Added .py file for Insertion sort, did some commenting.

---
+++ Insertion_Sort.py
@@ -0,0 +1,15 @@
+# Python 3
+# Here is my Code:
+
+# Introduction to Insertion Sort
+
+# Insertion Sort is not a fast sorting algorithms because it uses
+# nested loops to sort.
+
+# It is useful only for small dat sets.
+
+# It runs in O(n^2).
+
+
+
+

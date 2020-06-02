c84d465edb4b31fe7f0e6cfa7f3080bfb5091f46
AdityaSawant0912 <arsawant09@gmail.com>
2020-06-02 20:04:36 +0530

    Adding main function of Sorting Algorithm. And it Works!!

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -9,3 +9,21 @@
 
 # It runs in O(n^2).
 
+# Here is my Code:
+
+
+# Define a function:
+def selection_sort(arr):
+    for i in range(0, len(arr)-1):
+        min_index = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        if min_index != i:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+# Lets consider a example
+arr1 = [9, 4, 6, 2, 5, 1]
+print('Output : ', selection_sort(arr1))

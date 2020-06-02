b6e9863a6bc5bded87abca444cf3517f5deefb98
AdityaSawant0912 <arsawant09@gmail.com>
2020-06-02 19:57:05 +0530

    Adding a new .py file for Selection Sort, and commenting about it.

---
+++ Selection_Sort.py
@@ -0,0 +1,11 @@
+# Python 3
+
+# Introduction to Selection Sort
+
+# Selection Sort is not a fast sorting algorithms because it uses
+# nested loops to sort.
+
+# It is useful only for small dat sets.
+
+# It runs in O(n^2).
+

e864da181a87f2c36e3a24233377086a54ff7cac
AdityaSawant0912 <arsawant09@gmail.com>
2020-06-02 20:24:23 +0530

    Adding a new .py file for Bubble Sort, and commenting about it.

---
+++ Merge_Sort.py
@@ -0,0 +1,11 @@
+# Python 3
+
+# Introduction to Merge Sort
+
+# Merge Sort is recursive (method that calls itself)
+
+# Divide-and-conquer algorithm.
+
+# Very efficient for large data sets.
+
+# It runs in O(n*logn).

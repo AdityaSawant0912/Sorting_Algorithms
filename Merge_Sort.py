6670e11dab6623c81b703db89ca08468c53fe0bb
AdityaSawant0912 <arsawant09@gmail.com>
2020-06-02 20:27:47 +0530

    More Commenting

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -2,10 +2,16 @@
 
 # Introduction to Merge Sort
 
-# Merge Sort is recursive (method that calls itself)
+# Merge Sort is recursive (method that calls itself).
 
 # Divide-and-conquer algorithm.
 
 # Very efficient for large data sets.
+
+# Merge Sort does log n merge steps because each merge step
+# doubles the list size.
+
+# It does n work for each merge step because it must look at
+# every item.
 
 # It runs in O(n*logn).

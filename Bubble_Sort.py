3931b3ec51e19064cb502b0906edf54a9b327ced
AdityaSawant0912 <arsawant09@gmail.com>
2020-06-02 20:17:27 +0530

    Adding a .py file for Bubble Sort and commenting about it.

---
+++ Bubble_Sort.py
@@ -0,0 +1,14 @@
+# Python 3
+
+# Introduction to Insertion Sort
+
+# Insertion Sort is not a fast sorting algorithms because it uses
+# nested loops to sort.
+
+# It is useful only for small dat sets.
+
+# It runs in O(n^2).
+
+
+
+
